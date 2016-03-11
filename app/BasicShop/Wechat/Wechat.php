13fb0efb2bcbc1cdca5717953e8f1bf3c802abb0
PythonCakeMing <1851464029@qq.com>
2016-03-11 11:06:26 +0800

    basic arch

--- app/BasicShop/Wechat/Wechat.php
+++ app/BasicShop/Wechat/Wechat.php
@@ -8,12 +8,18 @@
 
 namespace App\BasicShop\Wechat;
 
+use Overtrue\Wechat\Server;
+use Overtrue\Wechat\Message;
+use Overtrue\Wechat\MenuItem;
+use Overtrue\Wechat\Menu;
+
+
 class Wechat
 {
 
     private $_appId;
     private $_secret;
-    private $_aeskey;
+    private $_aesKey;
     private $_token;
 
     function __construct()
@@ -21,20 +27,62 @@ class Wechat
         $this->_appId   = env('WX_APPID');
         $this->_secret  = env('WX_SECRET');
         $this->_token   = env('WX_TOKEN');
-        $this->_aeskey  = env('WX_ENCODING_AESKEY');
+        $this->_aesKey  = env('WX_ENCODING_AESKEY');
     }
 
+    public function getServer()
+    {
+        return new Server($this->_appId, $this->_token, $this->_aesKey);
+    }
 
     private function createMenuItem()
     {
+        return [
+            (new MenuItem("药械平台"))->buttons([
+                new MenuItem('入驻厂家', 'view', url('/platform/suppliers')),
+                new MenuItem('产品专区', 'view', url('/shop/products')),
+                new MenuItem('产品专区', 'view', url('/shop/personal')),
+            ]),
+            (new MenuItem("药械知识"))->buttons([
+                new MenuItem('最新资讯', 'view', url('/message/news')),
+                new MenuItem('直播讲堂', 'view', url('/message/video')),
+            ]),
+            (new MenuItem("药械社区"))->buttons([
+                new MenuItem('个人中心', 'view', url('/personal/information')),
+                new MenuItem('药械圈子', 'view', url('/personal/community')),
+                new MenuItem('药械论坛', 'view', url('/blog/index')),
+            ]),
+        ];
 
     }
 
     public function createMenu()
     {
+        $menuService = new Menu($this->_appId, $this->_secret);
+        $menus = $this->createMenuItem();
+        try {
+            $menuService->set($menus);
+        } catch(\Exception $e) {
+            return false;
+        } /*catch>*/
 
+        return true;
     }
 
+    public function messageEventCallback() {
+        return function ($message) {
+            return Message::make('text')->content('您好!');
+        };
+    }
+
+    public function subscribeEventCallback()
+    {
+        return function ($event) {
+            $openId = $event['FromUserName'];
+            $content = '嗨!欢迎关注药械通!';
+            return Message::make('text')->content($content);
+        };
+    }
 
 
 } /*class*/
---
+++ app/Http/Controllers/Message/MessageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Message;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class MessageController extends Controller
+{
+    //
+}

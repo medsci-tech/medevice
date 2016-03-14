92375b9892cbb5b2e607da369336ac29059ba554
PythonCakeMing <1851464029@qq.com>
2016-03-14 10:06:56 +0800

    update

--- app/BasicShop/Wechat/Wechat.php
+++ app/BasicShop/Wechat/Wechat.php
@@ -49,7 +49,6 @@ class Wechat
             (new MenuItem("药械社区"))->buttons([
                 new MenuItem('个人中心', 'view', url('/personal/information')),
                 new MenuItem('药械圈子', 'view', url('/personal/community')),
-                new MenuItem('药械论坛', 'view', url('/blog/index')),
             ]),
         ];
 

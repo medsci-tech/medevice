90fd5c538fa8264932a70108db90ffe6992140f8
Damon <whliujun@hotmail.com>
2016-03-15 17:02:02 +0800

    migration fix

--- database/migrations/2016_03_15_064733_create_products_table.php
+++ database/migrations/2016_03_15_064733_create_products_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTables extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,9 +12,6 @@ class CreateProductsTables extends Migration
      */
     public function up()
     {
-        //
-        Schema::create('products', function (Blueprint $table) {
-        });
     }
 
     /**

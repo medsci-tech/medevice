acdcef378807b515aaa8888c146b3c974fa35c69
Damon <whliujun@hotmail.com>
2016-03-24 17:59:35 +0800

    encoding

--- resources/views/personal/index.blade.php
+++ resources/views/personal/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="format-detection" content="telephone=no">
-    <title>������Ϣ</title>
+    <title>个人信息</title>
     <link rel="stylesheet" href="{{asset('/css/frozen.css')}}">
     <link rel="stylesheet" href="{{asset('/css/main.css')}}">
 </head>
@@ -12,36 +12,36 @@
 <div class="ui-container">
     <ul class="ui-list ui-list-text ui-border-tb ui-top">
         <li class="ui-border-t">
-            <h4 class="ui-nowrap">�û���</h4>
+            <h4 class="ui-nowrap">用户名</h4>
             <div class="ui-txt-info">{{$customer->nickname}}</div>
         </li>
         <li class="ui-border-t">
-            <h4 class="ui-nowrap">�û�����</h4>
-            <div class="ui-txt-info">��ͨ�û�</div>
+            <h4 class="ui-nowrap">用户类型</h4>
+            <div class="ui-txt-info">普通用户</div>
         </li>
     </ul>
 
     <ul class="ui-list ui-list-text ui-list-link ui-border-tb ui-top">
         <li class="ui-border-t">
-            <h4>�����¼</h4>
+            <h4>申请记录</h4>
         </li>
     </ul>
 
     <ul class="ui-list ui-list-text ui-list-link ui-border-tb ui-top">
         <li class="ui-border-t">
-            <h4>�ҵ��ղ�</h4>
+            <h4>我的收藏</h4>
         </li>
     </ul>
 
     <ul class="ui-list ui-list-text ui-list-link ui-border-tb ui-top">
         <li class="ui-border-t">
-            <h4>��ע����</h4>
+            <h4>关注厂家</h4>
         </li>
     </ul>
 
     <ul class="ui-list ui-list-text ui-list-link ui-border-tb ui-top">
         <li class="ui-border-t">
-            <h4>��ϵ�ͷ�</h4>
+            <h4>联系客服</h4>
         </li>
     </ul>
 </div>

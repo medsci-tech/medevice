817a59fdf4ca2d9740d9e4a51445188678f6d489
Damon <whliujun@hotmail.com>
2016-03-28 10:21:40 +0800

    migration

---
+++ database/migrations/2016_03_28_014922_create_table_supplier_follow_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableSupplierFollowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('supplier_follow', function (Blueprint $table) {
+            $table->increments('id')->comment('主键.');
+
+            $table->integer('customer_id')->unsigned()->comment('用户ID');
+            $table->foreign('customer_id')->references('id')->on('customers');
+
+            $table->integer('supplier_id')->unsigned()->comment('供应商ID');
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('supplier_follow');
+    }
+}

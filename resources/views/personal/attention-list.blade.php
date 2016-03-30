7e19486921b3432a43f9f802efb08ce460da05e2
Damon <whliujun@hotmail.com>
2016-03-30 14:52:33 +0800

    personal/supplier unfollow

--- resources/views/personal/attention-list.blade.php
+++ resources/views/personal/attention-list.blade.php
@@ -16,7 +16,9 @@
                 <div class="ui-list-info">
                     <h5>{{$attention->supplier->supplier_name}}</h5>
                 </div>
-                <h5 class="order_cancel"><a>取消关注</a></h5>
+                <h5 class="order_cancel">
+                    <button class="ui-btn ui-btn-danger" onclick="unfollow()">取消关注</button>
+                </h5>
             </li>
             <li class="ui-border-t">
                 <div class="ui-list-thumb">
@@ -31,6 +33,62 @@
         </ul>
     @endforeach
 </div>
-<div class="ui-txt-tips ui-txt-info ui-flex ui-flex-pack-center ui-top">已经没有更多关注商品了！</div>
+<div class="ui-dialog" id="dialog">
+    <div class="ui-dialog-cnt">
+        <header class="ui-dialog-hd ui-border-b">
+            <h3 id="dia_title"></h3>
+            <i class="ui-dialog-close" data-role="button" onclick="closeDia()"></i>
+        </header>
+        <div class="ui-dialog-bd">
+            <i class="ui-icon-success success_dia" id="icon"></i><h4 id="dia_content"></h4>
+        </div>
+        <div class="ui-dialog-ft">
+            <button type="button" data-role="button" onclick="closeDia()">确定</button>
+        </div>
+    </div>
+</div>
+
+<script src="http://cdn.bootcss.com/bootswatch/2.0.2/js/jquery.js"></script>
+<script src="{{asset('/js/zepto.min.js')}}"></script>
+<script src="{{asset('/js/frozen.js')}}"></script>
+
+<script>
+    function showDia(success, title, content) {
+        $("#dia_title").text(title);
+        $("#dia_content").text(content);
+        if (success) {
+            $("#icon").removeClass().addClass("ui-icon-success success_dia");
+        } else {
+            $("#icon").removeClass().addClass("ui-icon-success ui-txt-warning");
+        }
+        document.getElementById("dialog").style.display = "-webkit-box";
+    }
+    function closeDia() {
+        window.location.reload();
+        document.getElementById("dialog").style.display = "none";
+    }
+
+    function unfollow(supplier_id) {
+        $.ajax({
+            url: '/supplier/unfollow',
+            data: {
+                supplier_id: supplier_id
+            },
+            type: "get",
+            dataType: "json",
+            success: function (json) {
+                if (json.success) {
+                    showDia(true, '取消成功', '已取消关注！');
+                } else {
+                    showDia(true, '取消失败', '取消关注失败,请重试！');
+                }
+            },
+            error: function (xhr, status, errorThrown) {
+                console.log("Sorry, there was a problem!");
+                showDia(true, '关注失败', '关注失败,请重试！');
+            }
+        });
+    }
+</script>
 </body>
 </html>

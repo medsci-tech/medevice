e6cb6ff5a777ec52d9b4d052729d9ed9a4d5e887
Damon <whliujun@hotmail.com>
2016-04-01 17:56:11 +0800

    fix

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -13,10 +13,74 @@
         <li class="ui-border-t">
             <div class="ui-txt-info">MLC (multicore)一次性使用穿刺活检针</div>
         </li>
-
         <li class="ui-border-t">
             <video controls="controls"
-                   src="http://7xshr6.com1.z0.glb.clouddn.com/2"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/STERYLAB"
+                   width="100%" height="100%"
+                    >
+            </video>
+        </li>
+    </ul>
+
+    <ul class="ui-list ui-list-text ui-border-tb ui-txt-sub ui-top">
+        <li class="ui-border-t">
+            <div class="ui-txt-info">FLP(full-opty)一次性使用穿刺活检针</div>
+        </li>
+        <li class="ui-border-t">
+            <video controls="controls"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/STERYLAB"
+                   width="100%" height="100%"
+                    >
+            </video>
+        </li>
+    </ul>
+
+    <ul class="ui-list ui-list-text ui-border-tb ui-txt-sub ui-top">
+        <li class="ui-border-t">
+            <div class="ui-txt-info">BEN(ben)一次性使用穿刺活检针</div>
+        </li>
+        <li class="ui-border-t">
+            <video controls="controls"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/S%20ben"
+                   width="100%" height="100%"
+                    >
+            </video>
+        </li>
+    </ul>
+
+    <ul class="ui-list ui-list-text ui-border-tb ui-txt-sub ui-top">
+        <li class="ui-border-t">
+            <div class="ui-txt-info">MCN(mielo-can)一次性使用穿刺活检针1</div>
+        </li>
+        <li class="ui-border-t">
+            <video controls="controls"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/S%20mcn1"
+                   width="100%" height="100%"
+                    >
+            </video>
+        </li>
+    </ul>
+
+    <ul class="ui-list ui-list-text ui-border-tb ui-txt-sub ui-top">
+        <li class="ui-border-t">
+            <div class="ui-txt-info">MCN(mielo-can)一次性使用穿刺活检针2</div>
+        </li>
+        <li class="ui-border-t">
+            <video controls="controls"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/S%20mcn2"
+                   width="100%" height="100%"
+                    >
+            </video>
+        </li>
+    </ul>
+
+    <ul class="ui-list ui-list-text ui-border-tb ui-txt-sub ui-top">
+        <li class="ui-border-t">
+            <div class="ui-txt-info">BTT(btt)一次性使用穿刺活检针</div>
+        </li>
+        <li class="ui-border-t">
+            <video controls="controls"
+                   src="http://7xshr6.com1.z0.glb.clouddn.com/S%20btt"
                    width="100%" height="100%"
                     >
             </video>

b1fee0e69f4f0041ccab085f051a246492d55748
Damon <whliujun@hotmail.com>
2016-04-06 14:47:35 +0800

    fix

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -66,7 +66,7 @@
                 strHtml = '';
                 $(json.products).each(function () {
                     //strHtml += '<li class="ui-border-t"><div class="ui-list-thumb"><span style="background-image:url(http://placeholder.qiniudn.com/100x100)"></span></div><div class="ui-list-info"><a href="/shop/detail?id=' + this.id + '" class="ui-txt-default"><h4 class="ui-nowrap">' + this.name + '</h4><p class="ui-nowrap">' + this.introduction + '</p></a></div></li>';
-                    strHtml += '<li class="ui-border-t"><div class="ui-list-thumb"><span style="background-image:url(' + this.logo_image_url + ')"></span></div><div class="ui-list-info"><a href="/shop/detail?id=' + this.id + '" class="ui-txt-default"><h6 class="">' + this.name + '</h6><div class="ui-label-list"><label class="ui-label-s ui-nowrap"style="color:#6caf61;border-color: #6caf61;">' + this.introduction + '</label></div></a></div></li>';
+                    strHtml += '<li class="ui-border-t"><div class="ui-list-thumb"><span style="background-image:url(' + this.logo_image_url + ')"></span></div><div class="ui-list-info"><a href="/shop/detail?id=' + this.id + '" class="ui-txt-default"><h6 class="">' + this.name + '</h6><div class="ui-badge-muted" style="background:#18B4ED;">标签标签标签标签</div></a></div></li>';
                 });
                 $("#tbc_" + id).html(strHtml);
             },

fd69a753f81d6d921a4de9e1eb41449574372262
Damon <whliujun@hotmail.com>
2016-04-06 12:12:32 +0800

    fix

--- resources/views/supplier/index.blade.php
+++ resources/views/supplier/index.blade.php
@@ -9,17 +9,17 @@
     <link rel="stylesheet" href="{{asset('/css/main.css')}}">
 </head>
 <body>
-<div class="ui-slider">
-    <ul class="ui-slider-content" style="width: 300%">
-        <li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>
-        <li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>
-        <li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>
-    </ul>
-</div>
+{{--<div class="ui-slider">--}}
+{{--<ul class="ui-slider-content" style="width: 300%">--}}
+{{--<li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>--}}
+{{--<li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>--}}
+{{--<li><span style="background-image:url(http://placeholder.qiniudn.com/640x200)"></span></li>--}}
+{{--</ul>--}}
+{{--</div>--}}
 
 <div>
     <section class="ui-panel">
-        {{--<h2 class="ui-arrowlink">厂家<span class="ui-panel-subtitle">1328家</span></h2>--}}
+        <h2>厂家列表</h2>
         @foreach($suppliers as $supplier)
             <div class="list_img">
                 <div class="ui-border-radius">
@@ -38,23 +38,23 @@
 <script src="{{asset('/js/zepto.min.js')}}"></script>
 <script src="{{asset('/js/frozen.js')}}"></script>
 <script>
-    (function(){
-        var slider = new fz.Scroll('.ui-slider', {
-            role: 'slider',
-            indicator: true,
-            autoplay: true,
-            interval: 3000
-        });
-
-        slider.on('beforeScrollStart', function(from, to) {
-            console.log(from, to);
-        });
-
-        slider.on('scrollEnd', function(cruPage) {
-            console.log(curPage);
-        });
-
-    })();
+    //    (function(){
+    //        var slider = new fz.Scroll('.ui-slider', {
+    //            role: 'slider',
+    //            indicator: true,
+    //            autoplay: true,
+    //            interval: 3000
+    //        });
+    //
+    //        slider.on('beforeScrollStart', function(from, to) {
+    //            console.log(from, to);
+    //        });
+    //
+    //        slider.on('scrollEnd', function(cruPage) {
+    //            console.log(curPage);
+    //        });
+    //
+    //    })();
 </script>
 </body>
 </html>

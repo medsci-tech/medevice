9073b94b4a6a9950e9dc190502912eca0e7925fa
Damon <whliujun@hotmail.com>
2016-04-06 15:21:37 +0800

    fix

--- resources/views/shop/detail.blade.php
+++ resources/views/shop/detail.blade.php
@@ -9,15 +9,15 @@
     <link rel="stylesheet" href="{{asset('/css/main.css')}}">
 </head>
 <body>
-@if($product->bannerImages()->first())
-    <div class="ui-slider">
-        <ul class="ui-slider-content" style="width: 300%">
-            @foreach($product->bannerImages as $images)
-                <li><span style="background-image:url({{$images->image_url}})"></span></li>
-            @endforeach
-        </ul>
-    </div>
-@endif
+{{--@if($product->bannerImages()->first())--}}
+{{--<div class="ui-slider">--}}
+{{--<ul class="ui-slider-content" style="width: 300%">--}}
+{{--@foreach($product->bannerImages as $images)--}}
+{{--<li><span style="background-image:url({{$images->image_url}})"></span></li>--}}
+{{--@endforeach--}}
+{{--</ul>--}}
+{{--</div>--}}
+{{--@endif--}}
 
 <div class="comm-padding">
     <h4>{{$product->name}}</h4>

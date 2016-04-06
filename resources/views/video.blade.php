008a327de99e0ce1c0ba520c313965c51fddadc2
Damon <whliujun@hotmail.com>
2016-04-06 12:11:07 +0800

    fix

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
-    <title>视频教程</title>
+    <title>视频讲堂</title>
     <link rel="stylesheet" href="{{asset('/css/weui.css')}}">
 </head>
 <body>

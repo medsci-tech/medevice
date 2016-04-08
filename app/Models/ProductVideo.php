afa1721db243fa7ab706e33ac4e307a5fda8cb30
Damon <whliujun@hotmail.com>
2016-04-08 10:29:46 +0800

    video page

--- app/Models/ProductVideo.php
+++ app/Models/ProductVideo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVideo extends Model
 {
     protected $table = 'product_videos';
+
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product', 'product_id');
+    }
 }
--- resources/views/video_before0408.blade.php
+++ resources/views/video_before0408.blade.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
     <title>视频讲堂</title>
-    <link rel="stylesheet" href="{{asset('/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('/css/weui.css')}}">
 </head>
 <body>
-<div>
 @foreach($videos as $video)
-        <div class="panel panel-default">
-            <div class="panel-header"><b>产品名称</b>：<br><span>{{$video->product->name}}</span></div>
-            <div class="panel-header"><a href="/shop/detail?id={{$video->product_id}}">查看该产品详情</a></div>
-            <div class="panel-body">
+    <div style="padding: 5px;">
+        <h3 style="width: 100%; text-align: center;">{{$video->video_name}}</h3>
+
+        <div class="weui_cells weui_cells_access">
             <video controls="controls"
                    src="{{$video->video_url}}"
                    width="100%" height="100%"
@@ -21,6 +20,5 @@
         </div>
     </div>
 @endforeach
-</div>
 </body>
 </html>

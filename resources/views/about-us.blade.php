3d65dbc4d9ef5907923bc727b6873bd896ac0dbf
Damon <whliujun@hotmail.com>
2016-04-20 09:23:09 +0800

    edit about-us

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -12,6 +12,10 @@
 <div class="ui-container">
     <div class="ui-list ui-list-text ui-border-tb ui-top">
         <li class="ui-border-b">
+            <img src="http://7xshr6.com1.z0.glb.clouddn.com/1%20(2).png" width="100%">
+            <img src="http://7xshr6.com1.z0.glb.clouddn.com/1.jpg" width="100%">
+        </li>
+        <li class="ui-border-b">
             <h5>
                 药械通将器械产品及技术的专业化信息汇集和整理，形成了国内最具权威的药械知识库，为医药器械厂家、代理商等提供专业的信息检索和产品培训。同时，药械通还创建了行业信用评价体系，提供代理交易和第三方服务（包含注册、专利、招投标等）。药械通平台得到了湖北省医疗器械协会、同济医学院公共卫生学院医药信息管理系、美中医学交流协会、迈德科技（新三板835334）的支持和协助，目前平台正处在建设和运营期，欢迎圈内同仁交流与合作。</h5>
         </li>
